52c80828713cb386a60502d1d939f41bcb118b00
Michael Gernaey <mgern@microsoft.com>
2018-08-06 18:48:18 -0400

    MGERN: Adding documentation for 1.8.1. Adding Health SDK Samples. Adding Utilities for retrieving Codeable Concept OptionSet Type Vaules and for Importing codeable concepts while mapping the type values into the codeable concepts entity

---
+++ samplecode/apisamples/Microsoft.Dynamics.Health.Samples/HealthcareService.cs
@@ -0,0 +1,194 @@
+﻿// =====================================================================
+//  This file is part of the Microsoft Dynamics Accelerator code samples.
+//
+//  Copyright (C) Microsoft Corporation.  All rights reserved.
+//
+//  This source code is intended only as a supplement to Microsoft
+//  Development Tools and/or on-line documentation.  See these other
+//  materials for detailed information regarding Microsoft code samples.
+//
+//  THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+//  KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+//  PARTICULAR PURPOSE.
+// =====================================================================
+
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+// =====================================================================
+//  This file is part of the Microsoft Dynamics Accelerator code samples.
+//
+//  Copyright (C) Microsoft Corporation.  All rights reserved.
+//
+//  This source code is intended only as a supplement to Microsoft
+//  Development Tools and/or on-line documentation.  See these other
+//  materials for detailed information regarding Microsoft code samples.
+//
+//  THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+//  KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+//  PARTICULAR PURPOSE.
+// =====================================================================
+
+namespace Microsoft.Dynamics.Health.Samples
+{
+    public class HealthcareService
+    {
+        #region Class Level Members
+
+        /// <summary>
+        /// Stores the organization service proxy.
+        /// </summary>
+        private OrganizationServiceProxy _serviceProxy;
+
+        #endregion Class Level Members
+
+        #region How To Sample Code
+        /// <summary>
+        /// Create and configure the organization service proxy.
+        /// Initiate the method to create any data that this sample requires.
+        /// Create a healthcare service. 
+        /// </summary>
+        public void Run(ServerConnection.Configuration serverConfig, bool promptforDelete)
+        {
+            try
+            {
+                //<snippetMarketingAutomation1>
+                // Connect to the Organization service. 
+                // The using statement assures that the service proxy will be properly disposed.
+                using (_serviceProxy = new OrganizationServiceProxy(serverConfig.OrganizationUri, serverConfig.HomeRealmUri, serverConfig.Credentials, serverConfig.DeviceCredentials))
+                {
+                    // This statement is required to enable early-bound type support.
+                    _serviceProxy.EnableProxyTypes();
+
+                    Entity healthcareService = new Entity("msemr_healthcareservice");
+                    
+                    healthcareService["msemr_name"] = "Surgical Treatment";
+
+                    healthcareService["msemr_appointmentrequired"] = true;
+
+                    healthcareService["msemr_availabilityexceptions"] = "Public holiday availability";
+
+                    healthcareService["msemr_comment"] = "More details";
+
+                    healthcareService["msemr_healthcareservice"] = "Healthcare Service";
+
+                    healthcareService["msemr_extradetails"] = "Extra Details";
+
+                    healthcareService["msemr_notavailableduringenddatetime"] = DateTime.Now;
+                    healthcareService["msemr_notavailableduringstartdatetime"] = DateTime.Now;
+                    healthcareService["msemr_notavailabledescription"] = "Machine failure";
+
+                    healthcareService["msemr_eligibilitynote"] = "Eligibility Note";
+                    Guid eligibilityCodeableConceptId = SDKFunctions.GetCodeableConceptId(_serviceProxy, "Eligibile", 935000132);
+                    if (eligibilityCodeableConceptId != Guid.Empty)
+                    {
+                        healthcareService["msemr_eligibility"] = new EntityReference("msemr_codeableconcept", eligibilityCodeableConceptId);
+                    }
+
+                    Guid providedbyAccountId = Organization.GetAccountId(_serviceProxy, "Galaxy Corp");
+                    if (providedbyAccountId != Guid.Empty)
+                    {
+                        healthcareService["msemr_providedby"] = new EntityReference("account", providedbyAccountId);
+                    }
+
+                    Guid categoryCodeableConceptId = SDKFunctions.GetCodeableConceptId(_serviceProxy, "Category", 935000129);
+                    if (categoryCodeableConceptId != Guid.Empty)
+                    {
+                        healthcareService["msemr_category"] = new EntityReference("msemr_codeableconcept", categoryCodeableConceptId);
+                    }
+
+                    Guid healthcareServiceId = _serviceProxy.Create(healthcareService);
+
+                    // Verify that the record has been created.
+                    if (healthcareServiceId != Guid.Empty)
+                    {
+                        Console.WriteLine("Succesfully created {0}.", healthcareServiceId);
+                    }
+                }
+            }
+            // Catch any service fault exceptions that Microsoft Dynamics CRM throws.
+            catch (FaultException<Microsoft.Xrm.Sdk.OrganizationServiceFault>)
+            {
+                // You can handle an exception here or pass it back to the calling method.
+                throw;
+            }
+        }
+
+        #endregion How To Sample Code
+
+        #region Main Method
+
+        /// <summary>
+        /// Standard Main() method used by most SDK samples.
+        /// </summary>
+        /// <param name="args"></param>
+        static public void Main(string[] args)
+        {
+            try
+            {
+                // Obtain the target organization's Web address and client logon 
+                // credentials from the user.
+                ServerConnection serverConnect = new ServerConnection();
+                ServerConnection.Configuration config = serverConnect.GetServerConfiguration();
+
+                HealthcareService app = new HealthcareService();
+                app.Run(config, true);
+            }
+            catch (FaultException<Microsoft.Xrm.Sdk.OrganizationServiceFault> ex)
+            {
+                Console.WriteLine("The application terminated with an error.");
+                Console.WriteLine("Timestamp: {0}", ex.Detail.Timestamp);
+                Console.WriteLine("Code: {0}", ex.Detail.ErrorCode);
+                Console.WriteLine("Message: {0}", ex.Detail.Message);
+                Console.WriteLine("Plugin Trace: {0}", ex.Detail.TraceText);
+                Console.WriteLine("Inner Fault: {0}",
+                    null == ex.Detail.InnerFault ? "No Inner Fault" : "Has Inner Fault");
+            }
+            catch (System.TimeoutException ex)
+            {
+                Console.WriteLine("The application terminated with an error.");
+                Console.WriteLine("Message: {0}", ex.Message);
+                Console.WriteLine("Stack Trace: {0}", ex.StackTrace);
+                Console.WriteLine("Inner Fault: {0}",
+                    null == ex.InnerException.Message ? "No Inner Fault" : ex.InnerException.Message);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine("The application terminated with an error.");
+                Console.WriteLine(ex.Message);
+
+                // Display the details of the inner exception.
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine(ex.InnerException.Message);
+
+                    FaultException<Microsoft.Xrm.Sdk.OrganizationServiceFault> fe = ex.InnerException
+                        as FaultException<Microsoft.Xrm.Sdk.OrganizationServiceFault>;
+                    if (fe != null)
+                    {
+                        Console.WriteLine("Timestamp: {0}", fe.Detail.Timestamp);
+                        Console.WriteLine("Code: {0}", fe.Detail.ErrorCode);
+                        Console.WriteLine("Message: {0}", fe.Detail.Message);
+                        Console.WriteLine("Plugin Trace: {0}", fe.Detail.TraceText);
+                        Console.WriteLine("Inner Fault: {0}",
+                            null == fe.Detail.InnerFault ? "No Inner Fault" : "Has Inner Fault");
+                    }
+                }
+            }
+            // Additional exceptions to catch: SecurityTokenValidationException, ExpiredSecurityTokenException,
+            // SecurityAccessDeniedException, MessageSecurityException, and SecurityNegotiationException.
+
+            finally
+            {
+                Console.WriteLine("Press <Enter> to exit.");
+                Console.ReadLine();
+            }
+        }
+        #endregion Main method
+    }
+}
